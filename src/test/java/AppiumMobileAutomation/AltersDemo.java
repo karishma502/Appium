fd4d01c04987056c040f1013a269f7031d2e7b40
Karishma <karishmakate31@gmail.com>
2024-08-24 00:21:32 +0530

    #alerts Test

--- src/test/java/AppiumMobileAutomation/AltersDemo.java
+++ src/test/java/AppiumMobileAutomation/AltersDemo.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
@@ -32,5 +33,32 @@ public class AltersDemo extends BaseTest {
 		// alert implementation
 		// ok cancel dialog with a msg
 
+		driver.findElement(AppiumBy.accessibilityId("OK Cancel dialog with a message")).click();
+
+		//Alert alert = driver.switchTo().alert();
+
+		Thread.sleep(3000);
+		String alertMsg =driver.findElement(By.id("android:id/alertTitle")).getText();
+		String expected ="Lorem ipsum dolor sit aie consectetur adipiscing\n"
+				+ "Plloaso mako nuto siwuf cakso dodtos anr koop.";
+		Assert.assertEquals(alertMsg, expected);
+
+		driver.findElement(By.xpath("//android.widget.Button[@resource-id='android:id/button1']")).click();
+
+		// List Dialogue
+
+		driver.findElement(AppiumBy.accessibilityId("List dialog")).click();
+
+		driver.findElement(By.xpath("//android.widget.TextView[@resource-id='android:id/text1' and @text='Command one']")).click();
+
+		String cmdText = driver.findElement(By.id("android:id/message")).getText();
+		Assert.assertEquals(cmdText, "You selected: 0 , Command one");
+
+		driver.navigate().back();
+
+		// check progress Dialogue
+
+		driver.findElement(AppiumBy.accessibilityId("Progress dialog"));
+		Thread.sleep(3000);
 	}
 }
