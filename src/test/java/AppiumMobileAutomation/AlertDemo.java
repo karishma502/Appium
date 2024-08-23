08b46d8af26341b831c509ca8a941afe3ef1b91c
Karishma <karishmakate31@gmail.com>
2024-08-24 00:23:35 +0530

    #alerts Test

--- src/test/java/AppiumMobileAutomation/AlertDemo.java
+++ src/test/java/AppiumMobileAutomation/AlertDemo.java
@@ -1,31 +1,18 @@
 package AppiumMobileAutomation;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import com.google.common.collect.ImmutableMap;
-
 import io.appium.java_client.AppiumBy;
 
 
 
-public class AltersDemo extends BaseTest {
+public class AlertDemo extends BaseTest {
 
 	@Test
-	public void DragDropTest() throws MalformedURLException, URISyntaxException, InterruptedException {
+	public void alertTest() throws MalformedURLException, URISyntaxException, InterruptedException {
 
 		driver.findElement(AppiumBy.accessibilityId("App")).click();
 		driver.findElement(AppiumBy.accessibilityId("Alert Dialogs")).click();
